439152a5910e38d96612a53a0a73873b0cd77c43
Adam Stephen (astephen@kphd) <adam.stephen@ukaea.uk>
2020-06-04 17:42:22 +0200

    Updates from RHEL7

--- yocto-poky-rpi.sh
+++ yocto-poky-rpi.sh
@@ -1,18 +1,27 @@
 #!/usr/bin/env bash
-set x
+set -x
 echo "Download, configure, compile a basic raspberry pi image using yocto"
 echo "You need plenty of disk space for this."
 echo "As of 2020-05-22 choosing to use the dunfell branch 3.1 April 2020"
 echo "We assume the system has been configured in terms of prerequisite software."
 echo "my-bash-recipes intentionally excludes package installation and setup"
+#
+#
+echo "Preferred standard execution of this script is "
+echo "./yocto-poky-rpi.sh | tee yocto-poky-rpi-basic-dunfell-\$(date \"+%Y-%m-%d@%H:%M:%S\").log 2>&1"
 ###
 t0=$(date "+%s")
-#mkdir -p yocto-rpi-basic-dunfell && cd yocto-rpi-basic-dunfell
-#git clone git://git.yoctoproject.org/poky && cd poky
+mkdir -p yocto-rpi-basic-dunfell && cd yocto-rpi-basic-dunfell
+# On lin-css-kphd-0001 cannot access git: protocol but can use https
+# git clone git://git.yoctoproject.org/poky && cd poky
+git clone https://git.yoctoproject.org/git/poky && cd poky
+# git clone https://gitlab.com/yoctoproject/poky.git && cd poky
 export POKYTOPLEVEL=$(realpath $PWD)
-#git checkout dunfell
-#echo "Pull down the raspberry pi layer"
+git checkout dunfell
+echo "Pull down the raspberry pi layer"
 #git clone git://git.yoctoproject.org/meta-raspberrypi && cd meta-raspberrypi && git checkout dunfell & cd ../
+git clone https://git.yoctoproject.org/git/meta-raspberrypi && cd meta-raspberrypi && git checkout dunfell & cd ../
+pwd
 echo "Create a build directory"
 source oe-init-build-env rpi-dunfell-build 
 echo "Update the default build target"
