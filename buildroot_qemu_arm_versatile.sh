288ec675515360b2b9d6de3a8fa1620b1f369b98
Adam Stephen (H0207) <adam.stephen@ukaea.uk>
2020-06-14 09:45:19 +0100

    Fix relative paths.

--- buildroot_qemu_arm_versatile.sh
+++ buildroot_qemu_arm_versatile.sh
@@ -12,5 +12,5 @@ make -C ../buildroot O=$(pwd) qemu_arm_versatile_defconfig
 make
 # Create a driver script
 cat > run-qemu.sh <<EndOfQemuFile
-qemu-system-arm -M versatilepb -kernel output/images/zImage -dtb output/images/versatile-pb.dtb -drive file=output/images/rootfs.ext2,if=scsi -append "root=/dev/sda console=ttyAMA0,115200" -nographic
+qemu-system-arm -M versatilepb -kernel images/zImage -dtb images/versatile-pb.dtb -drive file=images/rootfs.ext2,if=scsi -append "root=/dev/sda console=ttyAMA0,115200" -nographic
 EndOfQemuFile
